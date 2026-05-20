457737f718afedfcad1415987a85df5b7b5c6b58
Fabian Ospina <maurospinarev@gmail.com>
2026-05-19 20:49:51 -0500

    Arreglo de nombres de archivos, implementacion del main y correccion de varios errores

---
+++ include/input.h
@@ -0,0 +1,41 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#ifdef _WIN32
+    #include <conio.h>
+#else
+    #include <termios.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <stdio.h>
+
+    inline int kbhit(void) {
+        struct termios oldt, newt;
+        int ch, oldf;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        fcntl(STDIN_FILENO, F_SETFL, oldf);
+        if (ch != EOF) { ungetc(ch, stdin); return 1; }
+        return 0;
+    }
+
+    inline int getch(void) {
+        struct termios oldt, newt;
+        int ch;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return ch;
+    }
+#endif
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,87 @@
-#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "game.h"
+#include "renderer.h"
+#include "input.h"
 
-int main(){
-    std::cout << "no hay nada jajaja";
+int main() {
+    srand(time(NULL));
+    char playAgain = 'y';
+    
+    while (playAgain == 'y' || playAgain == 'Y') {
+        GameState game;
+        initGame(&game);
+        
+        clearScreen();
+        drawTitleScreen();
+        drawHighScores(game.topScores);
+        printf("\n  Presiona cualquier tecla para continuar...");
+        while (!kbhit()) usleep(100000);
+        getch();
+
+        drawDifficultyMenu();
+        char diff = '2';
+        while (1) {
+            if (kbhit()) {
+                diff = getch();
+                if (diff >= '1' && diff <= '3') break;
+            }
+            usleep(100000);
+        }
+        setDifficulty(&game, diff - '1');
+
+        clearScreen();
+        printf(COLOR_CYAN "\n  PREPARANDO MAZMORRA...\n" COLOR_RESET);
+        usleep(1000000);
+
+        while (isGameRunning(&game)) {
+            if (kbhit()) {
+                int input = getch();
+                if (input == 27) {
+                    getch();
+                    input = getch();
+                    if (input == 'A') input = KEY_UP;
+                    else if (input == 'B') input = KEY_DOWN;
+                    else if (input == 'C') input = KEY_RIGHT;
+                    else if (input == 'D') input = KEY_LEFT;
+                }
+                
+                if (input == 'm' || input == 'M') {
+                    drawMinimap(game.rooms, &game.player);
+                    while (!kbhit()) usleep(100000);
+                    getch();
+                } else {
+                    processInput(&game, input);
+                }
+            }
+            
+            updateGame(&game);
+            clearScreen();
+            drawRoom(game.rooms, &game.player, game.enemies, TOTAL_ENEMIES);
+            drawHUD(&game.player, game.player.currentRoom);
+            if (strlen(game.lastMessage) > 0) drawMessage(game.lastMessage);
+            usleep(100000); 
+        }
+
+        clearScreen();
+        if (game.state == STATE_WIN) drawVictory();
+        else drawGameOver();
+        
+        drawStats(&game);
+        
+        printf("\n  " COLOR_BOLD "Quieres volver a jugar? (s/n): " COLOR_RESET);
+        char buffer[10];
+        if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+            playAgain = buffer[0];
+            if (playAgain == 's') playAgain = 'y';
+        } else {
+            playAgain = 'n';
+        }
+    }
+    
+    printf("\n  Gracias por jugar a Dungeon Escape Master Edition\n");
+    return 0;
 }
